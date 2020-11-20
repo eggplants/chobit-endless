bf42b1f5d21687e7bf69bb4eeb065e6d5938b9e6
eggplants <w10776e8w@yahoo.co.jp>
2020-11-20 14:37:18 +0900

    init

---
+++ pages/player.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <div>
+      <b-list-group>
+        <b-list-group-item
+          v-for="track in tracks.data.v1_endpoints"
+          :key="track"
+        >
+          {{ track }}
+        </b-list-group-item>
+      </b-list-group>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      tracks: [],
+    }
+  },
+  methods: {},
+  async asyncData({ app }) {
+    const baseUrl = 'https://dojinvoice-api.herokuapp.com/'
+    const response = await app.$axios.$get(baseUrl)
+    return { tracks: response }
+  },
+}
+</script>
+
+<style scoped></style>
