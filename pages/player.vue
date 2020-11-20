af2b8c06b689009a6c214ef47e10fdfd182d8d9e
eggplants <w10776e8w@yahoo.co.jp>
2020-11-20 15:40:08 +0900

    Add

--- pages/player.vue
+++ pages/player.vue
@@ -3,16 +3,19 @@
     <div>
       <h1>Player</h1>
       <h2>Now playing</h2>
+      <iframe
+        width="740"
+        height="215"
+        :src="chobit_links[getRandomInt(chobit_links.length)].chobit_link"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
       <h2>Track list</h2>
-      {{ response }}
-      <!-- <b-list-group>
-        <b-list-group-item
-          v-for="track in tracks.data.v1_endpoints"
-          :key="track"
-        >
-          {{ track }}
+      <b-list-group>
+        <b-list-group-item v-for="link in chobit_links" :key="link.id">
+          {{ link.id }}
         </b-list-group-item>
-      </b-list-group> -->
+      </b-list-group>
     </div>
   </section>
 </template>
@@ -24,11 +27,17 @@ export default {
       playing: true,
     }
   },
-  methods: {},
+  methods: {
+    getRandomInt(max) {
+      // ランダムな配列
+      return Math.floor(Math.random() * Math.floor(max))
+    },
+  },
   async asyncData({ app }) {
     const baseUrl = 'https://dojinvoice-api.herokuapp.com/'
-    const response = await app.$axios.$get(baseUrl)
-    return { response }
+    const endPoint = 'v1/chobit'
+    const response = await app.$axios.$get(baseUrl + endPoint)
+    return { chobit_links: response.data.data }
   },
 }
 </script>
