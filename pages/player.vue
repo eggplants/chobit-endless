9a6190d4711eafcf58c75379a898857bdf420e49
eggplants <w10776e8w@yahoo.co.jp>
2020-11-20 18:10:46 +0900

    Update

--- pages/player.vue
+++ pages/player.vue
@@ -7,15 +7,16 @@
         width="740"
         height="215"
         :src="chobit_links[getRandomInt(chobit_links.length)].chobit_link"
+        @load="playerLoaded"
         frameborder="0"
         allowfullscreen
       ></iframe>
       <h2>Track list</h2>
-      <b-list-group>
-        <b-list-group-item v-for="link in chobit_links" :key="link.id">
-          {{ link.id }}
-        </b-list-group-item>
-      </b-list-group>
+      <b-table
+        sticky-header
+        :items="chobit_links"
+        head-variant="light"
+      ></b-table>
     </div>
   </section>
 </template>
@@ -34,10 +35,21 @@ export default {
     },
   },
   async asyncData({ app }) {
-    const baseUrl = 'https://dojinvoice-api.herokuapp.com/'
-    const endPoint = 'v1/chobit'
-    const response = await app.$axios.$get(baseUrl + endPoint)
-    return { chobit_links: response.data.data }
+    const baseUrl = 'https://dojinvoice-api.herokuapp.com'
+    const chobitEndPoint = `/v1/chobit?index=${Math.floor(
+      Math.random() * Math.floor(20)
+    )}&offset=20`
+    const chobitLinks = await app.$axios.$get(baseUrl + chobitEndPoint)
+    const chobitLinksResponse = chobitLinks.data.data
+    const idConvEndPoint = '/v1/ids_to_data?ids='
+    var idlist = []
+    for (var i = 0; i < chobitLinksResponse.length; i++) {
+      idlist.push(chobitLinksResponse[i].id)
+    }
+    const detailData = await app.$axios.$get(
+      baseUrl + idConvEndPoint + idlist.join(',')
+    )
+    return { chobit_links: chobitLinksResponse, detail_data: detailData }
   },
 }
 </script>
